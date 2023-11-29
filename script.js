5bebd0702b43e4dc5faa83ab16d0746486e2d4fd
sana102 <144622870+sana102@users.noreply.github.com>
2023-11-29 16:24:48 -0500

    updates

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+// Your JavaScript code goes here
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 const completeAllButton = document.getElementById("complete-all-button");
+const clearButton = document.getElementById("clear-button");
 const goodJobMessage = document.getElementById("good-job-message");
+const remainingCountSpan = document.getElementById("remaining-count");
+const completedCountSpan = document.getElementById("completed-count");
 const todoApp = document.querySelector('.todo-app');
 const makeListButton = document.getElementById("make-list-button");
+let audio;
+
+let remainingCount = 0;
+let completedCount = 0;
 
 makeListButton.addEventListener("click", function () {
   todoApp.style.display = 'block';
   showTask();
   hideMakeListButton();
+  playMusic();
+  updateCounters();
 });
 
 function addTask() {
@@ -24,6 +34,10 @@ function addTask() {
     inputBox.value = "";
     saveData();
     checkCompleteAllButton();
+    remainingCount++;
+    updateCounters();
+    enableCompleteButton(); // Enable the complete button
+    enableClearButton(); // Enable the clear button
   }
 }
 
@@ -31,11 +45,14 @@ listContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
     saveData();
-    checkCompleteAllButton();
+    enableClearButton(); // Enable the clear button
+    checkCompleteAllButton(); // Moved here to update the "Complete All" button after individual task completion
   } else if (e.target.tagName === "SPAN") {
-    removeTask(e.target.parentElement);
+    const isTaskChecked = e.target.parentElement.classList.contains("checked");
+    removeTask(e.target.parentElement, isTaskChecked);
     saveData();
-    checkCompleteAllButton();
+    enableClearButton(); // Enable the clear button
+    checkCompleteAllButton(); // Moved here to update the "Complete All" button after individual task completion
   }
 });
 
@@ -43,18 +60,9 @@ completeAllButton.addEventListener("click", function () {
   completeAllTasks();
 });
 
-function removeTask(task) {
-  task.remove();
-}
-
-function clearCompletedTasks() {
-  const completedItems = document.querySelectorAll("#list-container .checked");
-  completedItems.forEach(function (completedItem) {
-    removeTask(completedItem);
-  });
-  saveData();
-  checkCompleteAllButton();
-}
+clearButton.addEventListener("click", function () {
+  clearCompletedTasks();
+});
 
 function completeAllTasks() {
   const tasks = document.querySelectorAll("#list-container li");
@@ -62,8 +70,47 @@ function completeAllTasks() {
     task.classList.add("checked");
   });
   saveData();
-  checkCompleteAllButton();
   showGoodJobMessage();
+  completedCount += remainingCount;
+  remainingCount = 0;
+  updateCounters();
+
+  // Clear tasks from the UI after completing all
+  listContainer.innerHTML = "";
+
+  // Moved checkCompleteAllButton here to update the "Complete All" button after completing all tasks
+  checkCompleteAllButton();
+  enableClearButton(); // Enable the clear button
+}
+
+function removeTask(task, isTaskChecked) {
+  task.remove();
+  if (!isTaskChecked) {
+    remainingCount--;
+  } else {
+    completedCount--;
+  }
+  updateCounters();
+}
+
+function clearCompletedTasks() {
+  const completedItems = document.querySelectorAll("#list-container .checked");
+  completedItems.forEach(function (completedItem) {
+    removeTask(completedItem, true);
+  });
+  saveData();
+  enableClearButton(); // Enable the clear button
+  // Moved checkCompleteAllButton here to update the "Complete All" button after clearing completed tasks
+  checkCompleteAllButton();
+}
+
+function enableClearButton() {
+  const completedItems = document.querySelectorAll("#list-container .checked");
+  clearButton.disabled = completedItems.length === 0;
+}
+
+function enableCompleteButton() {
+  completeAllButton.style.display = "inline";
 }
 
 function saveData() {
@@ -81,14 +128,19 @@ function showTask() {
 function checkCompleteAllButton() {
   const tasks = document.querySelectorAll("#list-container li");
   const completedTasks = document.querySelectorAll("#list-container .checked");
-  completeAllButton.style.display = tasks.length === completedTasks.length ? "none" : "inline";
+  completeAllButton.style.display = tasks.length === completedTasks.length || tasks.length === 0 ? "none" : "inline";
+}
+
+function updateCounters() {
+  remainingCountSpan.textContent = remainingCount;
+  completedCountSpan.textContent = completedCount;
 }
 
 function showGoodJobMessage() {
   goodJobMessage.style.display = "block";
   setTimeout(function () {
     hideGoodJobMessage();
-  }, 1000); // Hide the message after 5 seconds
+  }, 1000);
 }
 
 function hideGoodJobMessage() {
@@ -97,4 +149,18 @@ function hideGoodJobMessage() {
 
 function hideMakeListButton() {
   makeListButton.style.display = "none";
+  stopMusic();
+}
+
+function playMusic() {
+  audio = new Audio("./hellokittysound.mp3");
+  audio.loop = true;
+  audio.play();
+}
+
+function stopMusic() {
+  if (audio) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
 }
